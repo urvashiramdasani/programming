f68b4523bb6a4aeb8c423c9ba63463e492336d27
Urvashi Ramdasani <54858768+urvashiramdasani@users.noreply.github.com>
2022-01-20 16:17:52 +0530

    Create perfect-number.sh

---
+++ Shell and OS/perfect-number.sh
@@ -0,0 +1,28 @@
+# Author : Urvashi Ramdasani
+# Employee ID : 1450
+# Linux Training Batch 1
+
+echo "Enter a number : "
+read num
+sum=1
+i=2
+while [ $(( $i * $i )) -le $num ]
+do
+        if [ $(( $num % $i )) -eq 0 ]
+        then
+                if [ $(( $i * $i)) -ne $num ]
+                then
+                        t=$(( $num / $i ))
+                        sum=$(( $sum + $i + $t ))
+                else
+                        sum=$(( $sum + $i ))
+                fi
+        fi
+        i=$(( $i + 1))
+done
+if [ $sum -eq $num ] && [ $num -ne 1 ]
+then
+        echo "The number is perfect"
+else
+        echo "The number is not perfect"
+fi
